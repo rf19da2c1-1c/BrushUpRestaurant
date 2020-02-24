b7befc7857650166495f189c1807b32c543223f2
Peter Levinsky <pele@easj.dk>
2020-02-24 08:42:53 +0100

    Brush 4 - collections

--- BrushupConsoleApp/BrushupWorker.cs
+++ BrushupConsoleApp/BrushupWorker.cs
@@ -5,6 +5,8 @@ namespace BrushupConsoleApp
 {
     internal class BrushupWorker
     {
+        MenuCard mcard = new MenuCard();
+
         public void Start()
         {
             // Two drink objects 
@@ -18,7 +20,7 @@ namespace BrushupConsoleApp
             d2.Price = d2.Price * 1.1; // set price 10% higher
             Console.WriteLine(d2);
 
-            MenuCard mcard = new MenuCard();
+            
             mcard.Drinks.Add(d1);
             mcard.Drinks.Add(d2);
 
@@ -67,6 +69,88 @@ namespace BrushupConsoleApp
             }
 
             Console.WriteLine(d3);
+
+            /*
+             * Brush UP 4
+             *
+             */
+            PopulateDrinks();
+            PopulateDish();
+
+            Console.WriteLine("Most expensice drink");
+            Console.WriteLine(mcard.GetTheMostExpensiveDrink());
+
+            Console.WriteLine("Most cheapest drink");
+            Console.WriteLine(mcard.GetTheCheapestDrink());
+
+            Console.WriteLine("cheapest redwine");
+            Console.WriteLine(mcard.GetTheCheapestDrink("redwine"));
+
+            Console.WriteLine("All type of drinks");
+            Console.WriteLine(String.Join(", ", mcard.GetTypeOfDrinks()));
+
+            Console.WriteLine("cheapest of each type");
+            Console.WriteLine(String.Join(", ", mcard.GetTheCheapestDrinks()));
+
+            Console.WriteLine("Non alcoholic drinks");
+            Console.WriteLine(String.Join(", ", mcard.GetNonAlcoholicDrinks()));
+
+            Console.WriteLine("Price of cheapest Dish");
+            Console.WriteLine(mcard.PriceOfCheapestDish());
+
+            Console.WriteLine("Price of cheapest starter");
+            Console.WriteLine(mcard.PriceOfCheapestDish("starter"));
+            
+            Console.WriteLine("The average price of dish");
+            Console.WriteLine(mcard.GetAverageDishPrice());
+
+            Console.WriteLine("dishes starter, main, dessert");
+            Console.WriteLine(String.Join(", ", mcard.GetDishOfType()));
+
+            Console.WriteLine("2 starter");
+            Console.WriteLine(String.Join(", ", mcard.GetDishes("starter",2)));
+
+            Console.WriteLine("3 main");
+            Console.WriteLine(String.Join(", ", mcard.GetDishes("main", 3)));
+
+            Console.WriteLine("3 cheapest main");
+            Console.WriteLine(String.Join(", ", mcard.GetCheapestDishes("main", 3)));
+
+
+
+        }
+
+        private void PopulateDish()
+        {
+            mcard.Dishes.Add(new Dish("ministrone", "starter", 46));
+            mcard.Dishes.Add(new Dish("ribeye", "main", 158));
+            mcard.Dishes.Add(new Dish("lasagne", "main", 95));
+            mcard.Dishes.Add(new Dish("ice", "dessert", 38));
+            mcard.Dishes.Add(new Dish("crepesuzette", "dessert", 55));
+            mcard.Dishes.Add(new Dish("bananasplit", "dessert", 61));
+            mcard.Dishes.Add(new Dish("scrimps", "starter", 71));
+            mcard.Dishes.Add(new Dish("lopster", "starter", 137));
+            mcard.Dishes.Add(new Dish("foiegras", "starter", 110));
+            mcard.Dishes.Add(new Dish("chicken", "main", 98));
+            mcard.Dishes.Add(new Dish("meetloaf", "main", 88));
+            mcard.Dishes.Add(new Dish("lambSteak", "main", 132));
+
+        }
+
+        private void PopulateDrinks()
+        {
+            mcard.Drinks.Add(new Drink("rioja", "redwine", true, 56.8));
+            mcard.Drinks.Add(new Drink("bourgone", "redwine", true, 49.5));
+            mcard.Drinks.Add(new Drink("bordeaux", "redwine", true, 116.8));
+            mcard.Drinks.Add(new Drink("alsace", "whitewine", true, 72.8));
+            mcard.Drinks.Add(new Drink("chile", "whitewine", true, 49));
+            mcard.Drinks.Add(new Drink("cablis", "whitewine", true, 93.2));
+            mcard.Drinks.Add(new Drink("provance", "rosewine", true, 70));
+            mcard.Drinks.Add(new Drink("perrier", "water", false, 22));
+            mcard.Drinks.Add(new Drink("rioja riserva", "redwine", true, 68));
+            mcard.Drinks.Add(new Drink("fanta", "softdrink", false, 18));
+            mcard.Drinks.Add(new Drink("sprite", "softdrink", false, 18));
+
         }
     }
 }
--- RestaurantModelLib/model/MenuCard.cs
+++ RestaurantModelLib/model/MenuCard.cs
@@ -38,5 +38,211 @@ namespace RestaurantModelLib.model
 
             return $"{nameof(Drinks)}: [{drinkStr}], {nameof(Dishes)}: [{dishStr}]";
         }
+
+        /*
+         * Brush up 4
+         */
+        public Drink GetTheMostExpensiveDrink()
+        {
+            Drink drink = new Drink();
+
+            foreach (Drink d in _drinks)
+            {
+                if (d.Price > drink.Price)
+                {
+                    drink = d;
+                }
+            }
+
+            return drink;
+        }
+
+        public Drink GetTheCheapestDrink()
+        {
+            Drink drink = new Drink();
+            drink.Price = Double.MaxValue;
+
+            foreach (Drink d in _drinks)
+            {
+                if (d.Price < drink.Price)
+                {
+                    drink = d;
+                }
+            }
+
+            return drink;
+        }
+
+        public Drink GetTheCheapestDrink(String TypeOfDrink)
+        {
+            Drink drink = new Drink();
+            drink.Price = Double.MaxValue;
+
+            foreach (Drink d in _drinks)
+            {
+                if (d.Price < drink.Price && d.TypeOfDrink == TypeOfDrink)
+                {
+                    drink = d;
+                }
+            }
+
+            return drink;
+        }
+
+        public List<String> GetTypeOfDrinks()
+        {
+            List<String> types = new List<string>();
+
+            foreach (Drink d in _drinks)
+            {
+                if (!types.Contains(d.TypeOfDrink))
+                {
+                    types.Add(d.TypeOfDrink);
+                }
+            }
+
+            return types;
+        }
+
+        public List<Drink> GetTheCheapestDrinks()
+        {
+            List<Drink> drinks = new List<Drink>();
+            foreach (string typeOfDrink in GetTypeOfDrinks())
+            {
+                drinks.Add(GetTheCheapestDrink(typeOfDrink));
+            }
+
+            return drinks;
+        }
+
+        public List<Drink> GetNonAlcoholicDrinks()
+        {
+            List<Drink> drinks = new List<Drink>();
+            foreach (Drink d in _drinks)
+            {
+                if (!d.IsAlcoholic)
+                {
+                    drinks.Add(d);
+                }
+            }
+
+            return drinks;
+        }
+
+        public double PriceOfCheapestDish()
+        {
+            double cheapestPrice = Double.MaxValue;
+            foreach (Dish dish in _dishes)
+            {
+                if (dish.Price < cheapestPrice)
+                {
+                    cheapestPrice = dish.Price;
+                }
+            }
+
+            return cheapestPrice;
+        }
+
+        public double PriceOfCheapestDish(String TypeOfDish)
+        {
+            double cheapestPrice = Double.MaxValue;
+            foreach (Dish dish in _dishes)
+            {
+                if (dish.Price < cheapestPrice && dish.TypeOfDish == TypeOfDish)
+                {
+                    cheapestPrice = dish.Price;
+                }
+            }
+
+            return cheapestPrice;
+
+        }
+
+        public double GetAverageDishPrice()
+        {
+            double sum = 0;
+            foreach (Dish dish in _dishes)
+            {
+                sum = sum + dish.Price;
+            }
+
+            return _dishes.Count == 0 ? 0 : sum / _dishes.Count; // return 0 if no dish in _dishes otherwice the average
+        }
+
+        public List<Dish> GetDishOfType()
+        {
+            List<Dish> dishes = new List<Dish>();
+
+            List<Dish> mains = new List<Dish>();
+            List<Dish> desserts = new List<Dish>();
+
+            foreach (Dish d in _dishes)
+            {
+                switch (d.TypeOfDish.ToLower())
+                {
+                    case "starter":
+                        dishes.Add(d);
+                        break;
+                    case "main":
+                        mains.Add(d);
+                        break;
+                    case "dessert":
+                        desserts.Add(d);
+                        break;
+                }
+            }
+            dishes.AddRange(mains);
+            dishes.AddRange(desserts);
+
+            return dishes;
+        }
+
+        public List<Dish> GetDishes(String typeOfDish, int number)
+        {
+            List<Dish> dishes = new List<Dish>();
+
+            if (!(number == 0))
+            {
+                foreach (Dish d in _dishes)
+                {
+                    if (d.TypeOfDish == typeOfDish)
+                    {
+                        dishes.Add(d);
+
+                        // check if number have been reached 
+                        if (dishes.Count == number)
+                            break;
+                    }
+                }
+            }
+
+            return dishes;
+        }
+
+        public List<Dish> GetCheapestDishes(String typeOfDish, int number)
+        {
+            List<Dish> dishes = new List<Dish>();
+
+            if (number == 0)
+                return dishes;
+
+            // sort all dishes by price
+            SortedList<double,Dish> sortDishes = new SortedList<double,Dish>();
+            foreach (Dish d in _dishes)
+            {
+                if (d.TypeOfDish == typeOfDish)
+                {
+                    sortDishes.Add(d.Price, d);
+                }
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                dishes.Add(sortDishes.Values[i]);
+            }
+
+            return dishes;
+        }
+
     }
 }
